9655d5bca6150a2711f2e9488111545d91fdda4b
Your Name <you@example.com>
2014-11-14 11:40:59 +0000

    Update

--- Bowling Problem/Bowling1.rb
+++ Bowling Problem/Bowling1.rb
@@ -4,5 +4,6 @@ NoOfGames = gets.chomp.to_i
 if NoOfGames <= 50
 	puts ("Enter the date you joined: yyyy-mm-dd")
 	date = gets.chomp("yyyy-mm-dd")
-	puts Date.today - date
+	DateToday = puts Date.today
+	HowLong = DateToday - Date
 end
