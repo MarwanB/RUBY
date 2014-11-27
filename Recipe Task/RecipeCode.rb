28bc2e68ea30e6b2538611ebc68f0449f44624db
Your Name <you@example.com>
2014-11-27 11:38:30 +0000

    Recipe Task

--- Recipe Task/RecipeCode.rb
+++ Recipe Task/RecipeCode.rb
@@ -1,10 +1,17 @@
 puts ("Ingredient calculator for Pasta Bake")
 print("How many people are you serving: ")
-NumberOfPeople = gets.chomp
+NumberOfPeople = gets.chomp.to_i
 PastaBows = 50
 Milk = 125
 Cheese = 37.5
 
 PastaBowsServing = PastaBows * NumberOfPeople
+MilkServing = Milk * NumberOfPeople
+CheeseServing = Cheese * NumberOfPeople
 
-print("You need",PastaBowsServing,"grams of Pasta Bows")
+print("You need: ",PastaBowsServing," grams of Pasta Bows
+	  ")
+print(MilkServing," grams of Milk
+	  ")
+print(CheeseServing," grams of Cheese
+")
